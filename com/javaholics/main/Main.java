c5e5b3426e26375984d4bd4284f74d7d6ae549de
aurtom <artemmikhalov@i.ua>
2016-07-30 23:38:25 +0300

    sad

--- com/javaholics/main/Main.java
+++ com/javaholics/main/Main.java
@@ -3,9 +3,7 @@ package com.javaholics.main;
 public class Main {
 	public static void main (String[] args) {
 		System.out.println("Hello, javaholics!)");
-		//пушу с андроида
-		//пушай пушай
-		//new branch cration
-		//is this is a branch?
+
+		int [][] array = new int[20][20];
 	}
 }
